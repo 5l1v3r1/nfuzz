99fa73c70c90bf6212410b1dd6b2e5a542b2ec3c
root <root@DESKTOP-G2GJENH.localdomain>
2020-03-15 13:18:17 +0800

    CURL mode support

--- nfuzz.py
+++ nfuzz.py
@@ -6,7 +6,7 @@ import third.requests as requests
 import third.colorama as colorama
 from queue import Queue
 import platform 
-#import pdb
+# import pdb
 
 
 banner ='''
@@ -19,7 +19,7 @@ banner ='''
 author : n00B@khan
 '''
 
-# global IS_EXIT
+IS_EXIT = False
 headers = {
     'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
 }
@@ -36,6 +36,7 @@ class Brute:
         self.method = args.method
         self.thread_num = args.thread_num
         self.queue = Queue()
+        self.filter = args.filter
         
 
     def to_do(self):
@@ -60,8 +61,9 @@ class Brute:
                 t.join()
 
     def fuzz(self):
+        global IS_EXIT
         gc.collect()
-        while not self.queue.empty():
+        while not self.queue.empty() and IS_EXIT == False:
             if self.method.strip() == "post":
                 urls = self.queue.get()
                 resp = requests.post(urls,headers = headers,verify=False)
@@ -70,23 +72,34 @@ class Brute:
                 resp = requests.get(urls,headers = headers,verify=False)
             try:
                 if resp.status_code == 200 :
-                    sys.stdout.write('\r'+colorama.Fore.GREEN + '[+]\t200\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 403 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t403\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 302 :
-                    sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t302\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 301 :
-                    sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t301\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 405 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t405\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 400 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[-]\t400\t\t\t{}\n'.format(urls))
-                elif resp.status_code == 500 :
-                    sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t500\t\t\t{}\n'.format(urls))
+                    if self.filter == None or 200 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.GREEN + '[+]\t200\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 403:
+                    if self.filter == None or 403 not in self.filter:
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t403\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 302:
+                    if self.filter == None or 302 not in self.filter:
+                        sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t302\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 301:
+                    if self.filter == None or 301 not in self.filter:
+                        sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t301\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 405:
+                    if self.filter == None or 405 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t405\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 400:
+                    if self.filter == None or 400 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[-]\t400\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 500:
+                    if self.filter == None or 500 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t500\t\t\t{}\n'.format(urls))
+                elif resp.status_code == 404:
+                    if self.filter == None or 404 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t404\t\t\t{}\n'.format(urls))
+                    else:
+                        pass
             except Exception as e:
-                print(e)
-                pass
-                # sys.exit(1)
+                print("error")
+                sys.exit(1)
 
     def fuzz2(self):
         gc.collect()
@@ -94,24 +107,32 @@ class Brute:
             datas = self.queue.get()
             resp = requests.post(self.urls , headers = headers , data= datas ,verify=False)
             try:
-                if resp.status_code == 200 :
-                    sys.stdout.write('\r'+colorama.Fore.GREEN + '[+]\t200\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 403 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t403\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 302 :
-                    sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t302\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 301 :
-                    sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t301\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 405 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t405\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 400 :
-                    sys.stdout.write('\r'+colorama.Fore.CYAN  + '[-]\t400\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
-                elif resp.status_code == 500 :
-                    sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t500\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                if resp.status_code == 200:
+                    if self.filter == None or 200 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.GREEN + '[+]\t200\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 403:
+                    if self.filter == None or 403 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t403\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 302:
+                    if self.filter == None or 302 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t302\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 301:
+                    if self.filter == None or 301 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.BLUE  + '[+]\t301\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 405:
+                    if self.filter == None or 405 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[!]\t405\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 400:
+                    if self.filter == None or 400 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.CYAN  + '[-]\t400\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 500:
+                    if self.filter == None or 500 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t500\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
+                elif resp.status_code == 404:
+                    if self.filter == None or 404 not in self.filter :
+                        sys.stdout.write('\r'+colorama.Fore.RED   + '[-]\t500\t{}\t\t{}\n'.format(resp.headers['content-length'],datas))
             except Exception as e:
                 print("error")
-                self.queue.put(datas)
-                threading.Thread(target=self.fuzz2)
                 sys.exit(1)
 
 def main():
@@ -122,18 +143,35 @@ def main():
         from third.colorama import win32
     parser = argparse.ArgumentParser()
     flag_parser = parser.add_mutually_exclusive_group(required=False)
-    flag_parser.add_argument('-I',dest='show',action='store_true',help="CURL -I mode")
+    flag_parser.add_argument('-I',dest='CURL_I',action='store_true',help="CURL -I mode")
+    flag_parser.add_argument('-C',dest='CURL',action='store_true',help="CURL mode")
     parser.add_argument('-t',dest='thread_num',type=int,help="thread options",default=10)
     parser.add_argument('-u',dest='urls',type=str,help="url options")
     parser.add_argument('-w',dest='wordlists',type=str,help="wordlists options")
     parser.add_argument('-X',dest='method',type=str,help="http-method options",choices=['get','post'],default='get')
     parser.add_argument('-d',dest='data',type=str,help="post data")
+    parser.add_argument('--hc',dest='filter',type=int,help="http status code filter",nargs='*')
     args = parser.parse_args()
-    if args.show and args.urls:
-        resp = requests.get(args.urls,verify = False)
-        print(resp.status_code)
-        print(resp.headers)
-        sys.exit(1)
+    if args.CURL_I and args.urls:
+        if args.method.strip() == "get":
+            resp = requests.get(args.urls,verify = False)
+            print(resp.status_code)
+            print(resp.headers)
+            sys.exit(1)
+        elif args.method.strip() == "post":
+            resp = requests.post(args.urls , verify = False)
+            print(resp.status_code)
+            print(resp.headers)
+            sys.exit(1)
+    elif args.CURL and args.urls:
+        if args.method.strip() == "get":
+            resp = requests.get(args.urls , verify = False)
+            print(resp.text)
+            sys.exit(1)
+        elif args.method.strip() == "post":
+            resp = requests.post(args.urls , verify = False)
+            print(resp.text)
+            sys.exit(1)
     elif args.urls and args.wordlists and args.data != None:
         if "FUZZ" in args.data:
             brute = Brute(args)
@@ -153,9 +191,10 @@ def main():
         txt = '''
         -w Please enter the WORDLIST file address
         -t Please enter the THREAD number
-        -u Please enter the URL number
+        -u Please enter the URL number , usage:"http://www.baidu.com/FUZZ/error.html"
         -I CURL -I mode
-        -d Post data FUZZ need "FUZZ" word in data , data usage:"username=admin&password=FUZZ"
+        -C CURL mode
+        -d Post data  , usage:"username=admin&password=FUZZ"
         -X http-method support Post and Get (default)
         '''
         print(txt)
